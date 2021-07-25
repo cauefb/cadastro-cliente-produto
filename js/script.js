0f6ce44019d2150dae89cd4be7593e7c85018bb5
Cauê Ferreira <kau3hc@hotmail.com>
2021-07-25 14:17:32 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let cpf = document.getElementById("cpf").value;
+  let cep = document.getElementById("cep").value;
+  let rua = document.getElementById("rua").value;
+  let numero = document.getElementById("numero").value;
+  let bairro = document.getElementById("bairro").value;
+  let cidade = document.getElementById("cidade").value;
+  let estado = document.getElementById("estado").value;
+  let data = {
+    nome,
+    email,
+    cpf,
+    cep,
+    rua,
+    numero,
+    bairro,
+    cidade,
+    estado,
+  };
+
+  let convertData = JSON.stringify(data);
+
+  localStorage.setItem("lead", convertData);
+
+  let content = document.getElementById("content");
+
+  let carregando = `<p>Cadastrando...</p>`;
+  let pronto = `<p>Cliente cadastrado com sucesso </p>`;
+  let ok = "";
+
+  content.innerHTML = carregando;
+
+  setTimeout(() => {
+    content.innerHTML = pronto;
+  }, 1000);
+
+  setTimeout(() => {
+    content.innerHTML = ok;
+  }, 4000);
+  setTimeout(() => {
+    form.reset();
+  }, 3000);
+});
---
+++ js/scriptProduto.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("formProd");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let sku = document.getElementById("sku").value;
+  let nome = document.getElementById("nome").value;
+  let peso = document.getElementById("peso").value;
+  let altura = document.getElementById("altura").value;
+  let largura = document.getElementById("largura").value;
+  let descricao = document.getElementById("descricao").value;
+
+  let data = {
+    sku,
+    nome,
+    peso,
+    altura,
+    largura,
+    descricao,
+  };
+
+  let convertData = JSON.stringify(data);
+
+  localStorage.setItem("leadProd", convertData);
+
+  let content = document.getElementById("content");
+
+  let carregando = `<p>Cadastrando...</p>`;
+  let pronto = `<p>Produto cadastrado com sucesso </p>`;
+  let ok = "";
+
+  content.innerHTML = carregando;
+
+  setTimeout(() => {
+    content.innerHTML = pronto;
+  }, 1000);
+
+  setTimeout(() => {
+    content.innerHTML = ok;
+  }, 3000);
+  setTimeout(() => {
+    form.reset();
+  }, 3000);
+});
